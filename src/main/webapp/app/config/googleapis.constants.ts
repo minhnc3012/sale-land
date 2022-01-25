40a171a174f25af7032831b9c44e8429fd7b0cd9
minhn.nguyen <minh.nguyen@formos.com>
2022-01-25 10:49:11 +0700

    Update constants

--- src/main/webapp/app/config/googleapis.constants.ts
+++ src/main/webapp/app/config/googleapis.constants.ts
@@ -1,2 +1,2 @@
-export const GOOGLE_API_URL = 'AIzaSyDSw-TND6FewgVIcfZRf1148d6nb7pWt0s';
+export const GOOGLE_API_URL = 'https://maps.googleapis.com/maps/api/js';
 export const GOOGLE_API_KEY = 'AIzaSyDSw-TND6FewgVIcfZRf1148d6nb7pWt0s';
